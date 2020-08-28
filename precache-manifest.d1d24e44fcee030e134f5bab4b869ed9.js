a4b5036fb157af7ce9dc28806be15ea6bbb7d35d
Ammko128 <ammar.veljagic@gmail.com>
2020-08-28 21:14:51 +0200

    Updates

---
+++ precache-manifest.d1d24e44fcee030e134f5bab4b869ed9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56f944342cc169fa3ddb56b17656822d",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "758f0b47ef8af3f804f9",
+    "url": "/tic-tac-toe/static/css/main.134e2467.chunk.css"
+  },
+  {
+    "revision": "e3660b286d92f0993ec6",
+    "url": "/tic-tac-toe/static/js/2.141c23dc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe/static/js/2.141c23dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "758f0b47ef8af3f804f9",
+    "url": "/tic-tac-toe/static/js/main.d21f9cf7.chunk.js"
+  },
+  {
+    "revision": "b83c3f5dfddd51f16016",
+    "url": "/tic-tac-toe/static/js/runtime-main.e9088102.js"
+  }
+]);
